1384995dade669f3ba965487ceccdb5e569a909f
MAcDen34 <d.ngabo@alustudent.com>
2026-02-23 23:38:47 +0200

    Change the subs file name and also edited the outprint for 1-top file

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,11 +6,14 @@ import requests
 def top_ten(subreddit):
     """Queries Reddit API and prints titles of first 10 hot posts."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "linux:my.reddit.app:v1.0.0 (by /u/elie)"}
+    headers = {"User-Agent": "linux:api_advanced:v1.0"}
     params = {"limit": 10}
 
     response = requests.get(
-        url, headers=headers, params=params, allow_redirects=False
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False
     )
 
     if response.status_code != 200:
@@ -21,6 +24,4 @@ def top_ten(subreddit):
     posts = data.get("data", {}).get("children", [])
 
     for post in posts:
-        title = post.get("data", {}).get("title")
-        if title:
-            print(title)
+        print(post.get("data", {}).get("title"))
